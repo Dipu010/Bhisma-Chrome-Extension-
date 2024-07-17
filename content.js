6eff10b6b01dd3365487923ead168e21146f0157
Ryzen_10 <115201346+Dipu010@users.noreply.github.com>
2024-07-17 20:20:54 +0530

    URL data stored

--- content.js
+++ content.js
@@ -1,48 +1,48 @@
-console.log("From content page");
+// console.log("From content page");
 
-chrome.storage.local.get(["CountShortsYT"]).then((result) => {
-  const data = result.key;
-  console.log("Value is " + result.key);
-});
+// chrome.storage.local.get(["CountShortsYT"]).then((result) => {
+//   const data = result.key;
+//   console.log("Value is " + result.key);
+// });
 
-chrome.runtime.onMessage.addListener((obj, sender, response) => {
-  const { type, message } = obj;
+// chrome.runtime.onMessage.addListener((obj, sender, response) => {
+//   const { type, message } = obj;
 
-  if (type === "BLOCK_YT_SHORTS") {
-    // content.js
+//   if (type === "BLOCK_YT_SHORTS") {
+//     // content.js
 
-    console.log("YouTube Focus Mode script loaded.");
+//     console.log("YouTube Focus Mode script loaded.");
 
-    // Create a style element
-    const style = document.createElement("style");
-    style.textContent = `
-    #focus-mode-overlay {
-        position: fixed;
-        top: 0;
-        left: 240px;
-        width: 90%;
-        height: 100%;
-        background-color: rgba(0, 0, 0, 0.95);
-        color: white;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-size: 24px;
-        z-index: 10000;
-    }
-`;
-    document.head.appendChild(style);
+//     // Create a style element
+//     const style = document.createElement("style");
+//     style.textContent = `
+//     #focus-mode-overlay {
+//         position: fixed;
+//         top: 0;
+//         left: 240px;
+//         width: 90%;
+//         height: 100%;
+//         background-color: rgba(0, 0, 0, 0.95);
+//         color: white;
+//         display: flex;
+//         justify-content: center;
+//         align-items: center;
+//         font-size: 24px;
+//         z-index: 10000;
+//     }
+// `;
+//     document.head.appendChild(style);
 
-    // Create a focus mode overlay
-    const focusOverlay = document.createElement("div");
-    focusOverlay.id = "focus-mode-overlay";
-    focusOverlay.innerText = "You are in Focus Mode";
+//     // Create a focus mode overlay
+//     const focusOverlay = document.createElement("div");
+//     focusOverlay.id = "focus-mode-overlay";
+//     focusOverlay.innerText = "You are in Focus Mode";
 
-    // Append the overlay to the body
-    document.body.appendChild(focusOverlay);
+//     // Append the overlay to the body
+//     document.body.appendChild(focusOverlay);
 
-    console.log("Focus mode overlay added.");
+//     console.log("Focus mode overlay added.");
 
-    // You can add further actions here, like hiding the short or displaying a message
-  }
-});
+//     // You can add further actions here, like hiding the short or displaying a message
+//   }
+// });
--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,75 @@
-const count_Shorts=new Set();
+const count_Shorts = new Set();
 
-chrome.tabs.onUpdated.addListener((tabID,tab)=>{
-    if(tab.url && tab.url.includes("youtube.com/shorts")){
-        const queryParameters=tab.url.split("shorts/")[1];
-        //const urlParams= new URLSearchParams(queryParameters)
-        count_Shorts.add(queryParameters);
-        chrome.storage.local.set({ "CountShortsYT": count_Shorts }).then(() => {
-            console.log("Value is set");
-          });
-        console.log(count_Shorts);
+const UserEntryData = new Set();
 
-        if(count_Shorts.size>=3){
-            chrome.tabs.sendMessage(tabID,{
-                type:"BLOCK_YT_SHORTS",
-                message:"You are in focus mode"
+const User_TimeSpent_Data = new Set();
 
-            })
-            toggleMuteState(tabID)
-        }
+chrome.tabs.onUpdated.addListener((tabID, tab) => {
+  if (tab.url && tab.url.includes("youtube.com/shorts")) {
+    const queryParameters = tab.url.split("shorts/")[1];
+    //const urlParams= new URLSearchParams(queryParameters)
+    count_Shorts.add(queryParameters);
+    console.log(count_Shorts);
+    if (count_Shorts.size >= 3) {
+      blockYTShorts();
     }
-})
+  } else if (tab.url) {
+        StoreDataEntry();
+  }
+});
 
+async function blockYTShorts() {
+  chrome.tabs.update({ url: "https://www.youtube.com/" });
+  const tab = await getTab();
+  console.log(tab);
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: () => {
+      alert("You are in focus mode");
+    },
+  });
+}
+
+
+
+async function StoreDataEntry(){
+    let currentTab =await getTab();
+    console.log(currentTab)
+    const data = {
+      url: currentTab.url,
+      time: getTime(),
+    };
+    if(currentTab.title!=="New Tab" && currentTab.url!=="https://www.youtube.com/")
+        UserEntryData.add(data);
+    console.log(UserEntryData);
+}
 
 async function toggleMuteState(tabId) {
-    const tab = await chrome.tabs.get(tabId);
-    const muted = !tab.mutedInfo.muted;
-    await chrome.tabs.update(tabId, {muted});
-    console.log(`Tab ${tab.id} is ${muted ? "muted" : "unmuted"}`);
-  }
+  const tab = await chrome.tabs.get(tabId);
+  const muted = !tab.mutedInfo.muted;
+  await chrome.tabs.update(tabId, { muted });
+  console.log(`Tab ${tab.id} is ${muted ? "muted" : "unmuted"}`);
+}
+
+async function getTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return (tab);
+}
+
+function getAllTabs() {
+  chrome.tabs.query({}, function (tabs) {
+    tabs.forEach(function (tab) {
+      console.log(tab);
+    });
+  });
+}
+
+function getTime() {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, "0");
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+
+  const timeString = `${hours}:${minutes}`;
+  return timeString;
+}
